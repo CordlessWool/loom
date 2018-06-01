6064ce591a35cd2f1f7c877437786c3583fe8f7f
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2018-06-01 19:37:55 +0200

    update Header and add support for subscription and social icons

---
+++ assets/js/toggle.js
@@ -0,0 +1,26 @@
+/**
+ 
+ Scan document for ids beginning with toggle and add class hide to the element have the same id
+ 
+ */
+
+
+(function($){
+	
+	const regex = /toggle-([\w-_]*)/g;
+	
+	const buttons = $('[id^="toggle-"]');
+	for(var button of buttons){
+		$(button).on("click", function () {
+			console.log($(this).attr('id'));
+			var match = regex.exec($(this).attr('id'));
+			console.log(match);
+			if(match != null) {
+				var id = match[1];
+				console.log(id);
+				$('#' + id).toggleClass('hide');
+			}
+		})
+	}
+	
+})(window.jQuery);
